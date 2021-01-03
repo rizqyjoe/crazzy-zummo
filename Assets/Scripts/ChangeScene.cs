ce1d6c9b353ae5c2c2bb3c67ad9e8746f608df91
Rizqy Joe <rizqyjoe99@gmail.com>
2021-01-03 14:45:17 +0700

    initiation

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	public void btn_change(string name){
+		SceneManager.LoadScene(name);
+	}
+}
